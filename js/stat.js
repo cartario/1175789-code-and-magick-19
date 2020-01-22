f08351a269f5256d8af9a0572a269dd69650e070
Vasiliy <cartario@yandex.ru>
2020-01-22 08:35:22 +0300

    встроенные api, отрисовка имен и очков

---
+++ js/stat.js
@@ -0,0 +1,21 @@
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вы', 110, 95);
+  ctx.fillRect(140, 80, 350, 20);
+
+  ctx.fillText('Ура вы победили! ', 110, 40);
+  ctx.fillText('Список результатов: ', 110, 60);
+};
+
+
