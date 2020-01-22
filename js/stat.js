c08cfd28e3b2dbbe317c93da8b76c9838168cfc2
cartario <cartario@yandex.ru>
2020-01-22 11:49:02 +0300

    верт столбцы

--- js/stat.js
+++ js/stat.js
@@ -11,12 +11,24 @@ window.renderStatistics = function (ctx) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
-  ctx.fillStyle = '#000';
-  ctx.fillText('Вы', 110, 95);
-  ctx.fillRect(140, 80, 350, 20);
+  ctx.fillStyle = 'red';
+  ctx.fillText('Я', 150, 270);
+  ctx.fillRect(150, 250, 40, -100);
 
+  ctx.fillStyle = 'blue';
+  ctx.fillText('Кекс', 150+40+50, 270);
+  ctx.fillRect(150+40+50, 250, 40, -150);
+
+  ctx.fillStyle = 'lightgrey';
+  ctx.fillText('Катя', 150+40+50+50+40, 270);
+  ctx.fillRect(150+40+50+50+40, 250, 40, -50);
+
+  ctx.fillStyle = 'grey';
+  ctx.fillText('Игорь', 150+40+50+50+50+40+40, 270);
+  ctx.fillRect(150+40+50+50+50+40+40, 250, 40, -60);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили! ', 110, 40);
   ctx.fillText('Список результатов: ', 110, 60);
 };
-
-
